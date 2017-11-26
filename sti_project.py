7908fa8a8d2ad01c7e812fb4496e94198ce919fb
potbelliedpig29 <33005770+potbelliedpig29@users.noreply.github.com>
2017-11-26 14:35:18 -0800

    Update sti_project.py

--- sti_project.py
+++ sti_project.py
@@ -8,13 +8,17 @@ def openvideo():
 def openpicture():
     name= askopenfilename(filetypes=(("Image files", "*.png;*.jpeg;*.gif;.tif;.jpg"),("All files", "*.*")))
     file_name[0] = name
+# def hisdiff():
+#histogram difference (part 1)
+# def cpypix():
+#copy pixel method(part 2)
 ui = Tk()
 setrow = IntVar()
 setrow.set(0)
 ui.title("CMPT 365 Final Project")
-Label(ui, text="""Choose your video and which STI you want to work with:""", justify = LEFT, padx = 20).pack()
+Label(ui,text="Choose your video and which STI you want to work with:", justify = LEFT, padx = 20).pack()
 Button(text="Choose Video", width = 50, command=lambda : openvideo()).pack()
 Button(text="Choose Picture", width = 50, command=lambda : openpicture()).pack()
-Button(text="Histogram Difference", width = 50, command=lambda : hisint()).pack() 
+Button(text="Histogram Difference", width = 50, command=lambda : hisdiff()).pack() 
 Button(text="Copy Pixels", width = 50, command=lambda : cpypix()).pack()
 ui.mainloop()
